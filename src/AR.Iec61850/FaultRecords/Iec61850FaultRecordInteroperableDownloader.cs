18f6cdf2fd2bc15ff66b2550e4ff0e224e853fcb
masarray <ari.sulistiono@gmail.com>
2026-07-21 03:28:50 +0700

    Route fault-record downloads through adaptive FileOpen

--- src/AR.Iec61850/FaultRecords/Iec61850FaultRecordInteroperableDownloader.cs
+++ src/AR.Iec61850/FaultRecords/Iec61850FaultRecordInteroperableDownloader.cs
@@ -77,7 +77,7 @@ public static class Iec61850FaultRecordInteroperableDownloader
                                  bufferSize: 64 * 1024,
                                  useAsync: true))
                 {
-                    var transfer = await session.DownloadFileInteroperableAsync(
+                    var transfer = await session.DownloadFileAdaptiveAsync(
                         file.RemotePath,
                         output,
                         new MmsFileTransferOptions
